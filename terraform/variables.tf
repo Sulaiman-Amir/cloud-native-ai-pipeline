72d0a08befae1bc7ece8bef8f80af6089176c3bb
Sulaiman Amir <SulleMaan007@gmail.com>
2025-07-12 22:28:10 +0500

    add tf variables for deployment

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,21 @@
+variable "lambda_function_name" {
+  description = "Name of the Lambda function"
+  type        = string
+}
+
+variable "s3_bucket_name" {
+  description = "S3 bucket for storing models or data"
+  type        = string
+}
+
+variable "api_gateway_name" {
+  description = "Name of the API Gateway"
+  type        = string
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
 
