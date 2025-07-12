0339b8bbcbff1dc39f8097a53de04d17b2135e87
Sulaiman Amir <SulleMaan007@gmail.com>
2025-07-12 22:28:40 +0500

    add tf output values

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,10 @@
+output "lambda_function_arn" {
+  description = "ARN of the Lambda function"
+  value       = aws_lambda_function.inference_function.arn
+}
+
+output "api_endpoint" {
+  description = "URL of the API Gateway endpoint"
+  value       = aws_api_gateway_deployment.deployment.invoke_url
+}
 
