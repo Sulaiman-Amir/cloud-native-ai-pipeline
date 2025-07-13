2c182d10b2c63ea4d3be9793307b243ec760e302
Sulaiman Amir <SulleMaan007@gmail.com>
2025-07-14 03:05:03 +0500

    add SageMaker model training script

--- sagemaker/train_model.py
+++ sagemaker/train_model.py
@@ -1,1 +1,17 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+import joblib
+import os
+
+# Load dataset (dummy example)
+data = pd.read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv')
+X = data.drop('species', axis=1)
+y = data['species']
+
+# Train model
+clf = RandomForestClassifier()
+clf.fit(X, y)
+
+# Save model to file
+joblib.dump(clf, os.path.join('/opt/ml/model', 'model.joblib'))
 
