7cb4abb1831bac4184be996cb3ba9e3a44e81afb
Sulaiman Amir <SulleMaan007@gmail.com>
2025-07-14 03:05:52 +0500

    add script to deploy model endpoint

--- sagemaker/deploy_model.py
+++ sagemaker/deploy_model.py
@@ -1,1 +1,15 @@
+import boto3
+
+sm = boto3.client('sagemaker')
+
+response = sm.create_model(
+    ModelName='topg-rf-model',
+    PrimaryContainer={
+        'Image': '683313688378.dkr.ecr.us-east-1.amazonaws.com/sagemaker-scikit-learn:0.23-1-cpu-py3',
+        'ModelDataUrl': 's3://your-bucket-name/path-to/model.tar.gz'
+    },
+    ExecutionRoleArn='arn:aws:iam::your-account-id:role/SageMakerExecutionRole'
+)
+
+print("Model Deployed:", response['ModelArn'])
 
